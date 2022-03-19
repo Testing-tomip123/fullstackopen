0ce502696736edff0001d9ef9f2d8042aa212d37
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-19 20:20:10 +0200

    unicafe step4 COMPLETED

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <p>
