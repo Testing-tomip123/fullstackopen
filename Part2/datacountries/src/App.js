5a5a4b368459f2f5f1540aa423da7d2e5ed7e465
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-20 16:13:49 +0200

    Data for countries, step3 COMPLETED

--- Part2/datacountries/src/App.js
+++ Part2/datacountries/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Country = ({ country }) => {
   return (
-    <div>
+    <><div>
       <h1>{country[0].name}</h1>
       <div>Capital: {country[0].capital}</div>
       <div>Population: {country[0].population}</div>
@@ -12,14 +12,61 @@ const Country = ({ country }) => {
       <div>Region: {country[0].region}</div>
       <h2>Languages</h2>
       <ul>
-        {country[0].languages.map(language =>
-          <li key={language.name}>{language.name}</li>
+        {country[0].languages.map(language => <li key={language.name}>{language.name}</li>
         )}
       </ul>
       <img src={country[0].flag} alt={country[0].name} height='200' width='300' />
+    </div><div>
+        <Weather city={country[0].capital} />
+      </div></>
+  )
+}
+
+const Weather = ({ city }) => {
+  const [theWeather, setWeather] = useState([])
+  // api key = 37c865110d45a72b1f18c292be08683f
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [city])
+
+  return (
+    <div>
+      <h2>Weather in {city}</h2>
+      <div>
+        <b>Temperature: </b> {theWeather?.main?.temp} Celsius
+      </div>
+      <div>
+        <b>Cloudiness: </b> {theWeather?.clouds?.all} %
+      </div>
+      <div>
+        <b>Temperature min: </b> {theWeather?.main?.temp_min} Celsius
+      </div>
+      <div>
+        <b>Temperature max: </b> {theWeather?.main?.temp_max} Celsius
+      </div>
+      <div>
+        <b>Wind: </b> {theWeather?.wind?.speed} m/s
+      </div>
+      <div>
+        <b>Wind direction: </b> {theWeather?.wind?.deg} degrees
+      </div>
+      <div>
+        <b>Pressure: </b> {theWeather?.main?.pressure} hPa
+      </div>
+      <div> 
+        <b>Description: </b> {theWeather?.weather?.[0]?.description}
+      </div>
+      <div>
+        <img src={`http://openweathermap.org/img/wn/${theWeather?.weather?.[0].icon}.png`} alt={theWeather?.weather?.[0].description} height='100' width='100'/>
+      </div>
     </div>
   )
 }
+
 
 const Countries = ({ searchName, countriesShow, ShowCountry }) => {
   if (countriesShow.length === 1) {
