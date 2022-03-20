956e895b0bc84e420def931d0366199a352bc537
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-20 14:50:32 +0200

    Data for countries, step2 COMPLETED

--- Part2/datacountries/src/App.js
+++ Part2/datacountries/src/App.js
@@ -1,40 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Countries = ({ countries, countriesShow }) => {
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country[0].name}</h1>
+      <div>Capital: {country[0].capital}</div>
+      <div>Population: {country[0].population}</div>
+      <div>Area: {country[0].area} ㎢</div>
+      <div>Currency: {country[0].currencies[0].name}</div>
+      <div>Region: {country[0].region}</div>
+      <h2>Languages</h2>
+      <ul>
+        {country[0].languages.map(language =>
+          <li key={language.name}>{language.name}</li>
+        )}
+      </ul>
+      <img src={country[0].flag} alt={country[0].name} height='200' width='300' />
+    </div>
+  )
+}
+
+const Countries = ({ searchName, countriesShow, ShowCountry }) => {
   if (countriesShow.length === 1) {
+    return <Country country={countriesShow} />
+  } 
+  else if (countriesShow.length <= 10) {
     return (
       <div>
-        <h1>{countriesShow[0].name}</h1>
-        <div>Capital: {countriesShow[0].capital}</div>
-        <div>Population: {countriesShow[0].population}</div>
-        <div>Area: {countriesShow[0].area} ㎢</div>
-        <div>Currency: {countriesShow[0].currencies[0].name}</div>
-        <div>Region: {countriesShow[0].region}</div>
-        <h2>Languages</h2>
-        <ul>
-          {countriesShow[0].languages.map(language =>
-            <li key={language.name}>{language.name}</li>
-          )}
-        </ul>
-        <img src={countriesShow[0].flag} alt={countriesShow[0].name} height='200' width='200' />
+        {countriesShow.map(country =>
+          <div key={country.numericCode}>
+            {country.name}
+            <button type='button' value={country.name} onClick={ShowCountry}>Show</button>
+          </div>
+        )}
       </div>
     )
   }
-  else if (countries === undefined || countriesShow.length <= 10) {
+  else if (searchName === '') {
     return (
       <div>
-        {countriesShow.map((country) =>
+        {countriesShow.map(country =>
           <div key={country.numericCode}>
             {country.name}
-            </div>
+          </div>
         )}
       </div>
     )
   }
   else {
     return (
-      <div>Too many matches, specify another filter</div>
+      <div>
+        Too many matches, specify another filter
+      </div>
     )
   }
 }
@@ -42,6 +60,7 @@ const Countries = ({ countries, countriesShow }) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [searchName, setSearchName] = useState('')
+
 
   useEffect(() => {
     axios
@@ -55,6 +74,11 @@ const App = () => {
     setSearchName(event.target.value)
   }
 
+  const ShowCountry = (event) => {
+    event.preventDefault()
+    setSearchName(event.target.value)
+  }
+
   const countriesShow = countries.filter(country =>
     country.name.toLowerCase().includes(searchName.toLowerCase()))
 
@@ -63,14 +87,10 @@ const App = () => {
     <div>
       find countries <input value={searchName} onChange={handleSearch} />
     </div>
-    <Countries countries={searchName} countriesShow={countriesShow} />
+    <Countries countries={searchName} countriesShow={countriesShow} ShowCountry={ShowCountry} />
     </div>
   )
 }
  
-
-
-
-
 
 export default App;
