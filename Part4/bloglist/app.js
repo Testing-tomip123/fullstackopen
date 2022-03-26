d632e343cc426c4f7dfdbde81c10950406c2b271
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-26 20:08:05 +0200

    Blog list, step1 and step2.

---
+++ Part4/bloglist/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogrouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB', config.MONGODB_URI)
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(blogrouter)
+
+module.exports = app
+
