c3cc86948165c65f6fc60c74c178b7ed69ce9e56
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-27 17:46:27 +0300

    helper functions and unit tests, step3

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -7,7 +7,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return -1
+    }
+
+    let maxLikes = 0, position = 0
+    for(let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > maxLikes) {
+            maxLikes = blogs[i].likes
+            position = i
+        }
+    }
+    return blogs[position]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes, 
+    favoriteBlog
 }
