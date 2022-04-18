92f825466f9e3d7db2464e622d42c490d96fa923
Tomi Lahti <tomi.lahti2@gmail.com>
2022-04-18 22:27:31 +0300

    bloglist expansion, step7

--- Part4/bloglist/controllers/loginController.js
+++ Part4/bloglist/controllers/loginController.js
@@ -18,7 +18,7 @@ loginRouter.getLogin = async function(req, res, next) {
         id: user._id,
     }
 
-    const token = jsonwebtoken.sign(userForToken, process.env.SECRET)
+    const token = jsonwebtoken.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 })
 
     res.status(200).json({ token, username: user.username, name: user.name })
 }
